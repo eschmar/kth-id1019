570b3d57ab61aaa8996154c24af1889922deddad
Marcel Eschmann <marcel.eschmann@gmail.com>
2017-02-09 09:20:53 +0100

    Add linear fibonacci version

--- introduction/fibonacci.erl
+++ introduction/fibonacci.erl
@@ -10,6 +10,16 @@ fib(1) -> 1;
 fib(0) -> 0.
 
 %
+%   fibonacci, dynamic O(n)
+%
+
+fibb(0) -> {1, na};
+fibb(1) -> {1, 1};
+fibb(N) ->
+    {N1, N2} = fibb(N-1),
+    {N1+N2, N1}.
+
+%
 %   benchmarks
 %
 
@@ -17,8 +27,9 @@ bench() ->
     Ls = [8,10,12,14,16,18,20,22,24,26,28,30,32,34,36,38,40],
     N = 10,
     Bench = fun(L) ->
-        T = time(N, fun() -> fib(L) end),
-        io:format("n: ~4w fib(n) calculated in: ~8w us~n", [L, T])
+        Tn = time(N, fun() -> fib(L) end),
+        Tr = time(N, fun() -> fibb(L) end),
+        io:format("n: ~4w naive: ~8w us dyn: ~8w us~n", [L, Tn, Tr])
         end,
     lists:foreach(Bench, Ls).
 
