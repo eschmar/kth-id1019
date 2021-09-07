b419ffde38641ce7e0431ea9428dfb9e142832d6
Marcel Eschmann <marcel.eschmann@gmail.com>
2021-09-07 11:24:49 +0200

    Update compile script

--- mandelbrot/compile.sh
+++ mandelbrot/compile.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # Compile script for Mac OSX
-rm complex_nif.so;
-gcc -std=c99 -fPIC -shared -o complex_nif.so complex_nif.c -I /usr/local/lib/erlang/erts-8.2/include/ -flat_namespace -undefined suppress;
+rm -f complex_nif.so;
+gcc -std=c99 -fPIC -shared -o complex_nif.so complex_nif.c -I /usr/local/lib/erlang/erts-11.1.5/include/ -flat_namespace -undefined suppress;
